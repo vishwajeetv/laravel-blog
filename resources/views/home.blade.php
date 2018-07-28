8e6c22e9a99e619b7c99dd7fe802e993d81c3809
Vishwajeet Vatharkar <vishwajeet.vatharkar@vuclip.com>
2018-07-28 20:11:28 +0530

    showing all blog posts done.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,26 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        @if( Auth::check() )
+            <div class="col-md-8">
+                @if( Auth::user()->can('read-post') )
+                    @foreach ($posts as $post)
+                    <div class="card">
+                        <div class="card-header">{{$post['postTitle']}}</div>
+                        <div class="card-body">
+                            {{$post['postText']}}
                         </div>
-                    @endif
-
-                        @if( Auth::check() )
-                            @if( Auth::user()->can('read-post') )
-                                {{$posts}}
-                            @endif
-                        @endif
-                    You are logged in!
-                </div>
+                    </div>
+                    @endforeach
+                @endif
             </div>
-        </div>
+        @endif
     </div>
 </div>
 @endsection
